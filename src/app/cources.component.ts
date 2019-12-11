e771f740aed9fa085eb2f03c2d43e83a3d4c09ec
Stephen <stephenyoon84@gmail.com>
2019-12-11 18:07:58 -0500

    practice

--- src/app/cources.component.ts
+++ src/app/cources.component.ts
@@ -6,14 +6,16 @@ import { Component } from '@angular/core'
     selector: 'courses',
     template: `
         <h2>{{ getTitle() }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">{{ course }}</li>
-        </ul>
+        <img src="{{ imageUrl }}" />
+        
+        <h2 [textContent]="title"></h2>
+        <img [src]="imageUrl" />
     `
 })
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    imageUrl = "http://lorempixel.com/400/200";
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
