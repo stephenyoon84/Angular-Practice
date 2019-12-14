ec65e0772caba0594bcfe0b471ee6b370eb8fd82
Stephen <stephenyoon84@gmail.com>
2019-12-14 16:55:53 -0500

    practice pipes

--- src/app/cources.component.ts
+++ src/app/cources.component.ts
@@ -17,20 +17,31 @@ import { Component } from '@angular/core'
                 <td [attr.colspan]="colSpan"></td>
             </tr>
         </table>
-        <button (click)="onSave($event)" class="btn btn-primary" [class.active]="isActive">Save</button>
+        <button (click)="onSave($event)" class="btn btn-primary" [class.active]="isActive">Save</button> <br/>
+        {{ courses.title }} <br/>
+        {{ courses.students }} <br/>
+        {{ courses.rating | number }} <br/>
+        {{ courses.price }} <br/>
+        {{ courses.releaseDate }} <br/>
     `
 })
 export class CoursesComponent {
     title = "List of courses";
-    courses;
+    courses = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2019, 12, 14)
+    };
     imageUrl = "http://lorempixel.com/400/200";
     colSpan = 2;
     isActive = true;
     email = "me@example.com";
 
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-    }
+    // constructor(service: CoursesService) {
+    //     this.courses = service.getCourses();
+    // }
 
     getTitle(){
         return this.title;
